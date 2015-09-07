e202afa65c13209f0cf7bf9aa3503a8700e5c259
pravin2510 <pravin2510@gmail.com>
2015-09-07 10:41:02 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,50 @@
+(function() {
+	
+	var app=angular.module("sampleApp",[])
+  var ngContextMenu = angular.module('directive.contextMenu', []);
+
+  
+  ngContextMenu.directive('context', [
+
+    function() {
+      return {
+        restrict: 'A',
+        scope: '@&',
+        compile: function compile(tElement, tAttrs, transclude) {
+          return {
+            post: function postLink(scope, iElement, iAttrs, controller) {
+              var ul = $('#' + iAttrs.context),
+                last = null;
+
+              ul.css({
+                'display': 'none'
+              });
+              $(iElement).bind('contextmenu', function(event) {
+                event.preventDefault();
+                 ul.css({
+                  position: "fixed",
+                  display: "block",
+                  left: event.clientX + 'px',
+                  top: event.clientY + 'px'
+                });
+                last = event.timeStamp;
+              });
+           
+
+              $(document).click(function(event) {
+                var target = $(event.target);
+                if (!target.is(".popover") && !target.parents().is(".popover")) {
+                  if (last === event.timeStamp)
+                    return;
+                  ul.css({
+                    'display': 'none'
+                  });
+                }
+              });
+            }
+          };
+        }
+      };
+    }
+  ]);
+})(window.angular);
